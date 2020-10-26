912693603b7a1c51af25eb16efe34be0894b9f09
Alexis-Pel <alexispelissier2002@gmail.com>
2020-10-26 15:32:44 +0100

    annuaire

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-from art import *
+def create_contact(name, phone, is_favorite):
+    contact = {
+        'name': name,
+        'phone': phone,
+        'is_favorite': is_favorite,
+    }
+    return contact
+
+# Ajout du contact dans l'annuaire
+def add_contact(c):
+    phone = c['phone']
+    annuaire[phone] = c
 
 
-def say_hello(name):
-    print(text2art(f"Bonjour {name}"))
+def get_names():
+    names = []
+    for n in annuaire:
+        names.append(annuaire[n]["name"])
+
+    names.sort()
+    return names
 
 
-n = "Alexis"
-say_hello(n)
-print("test")
+
+my_name = 'Alexis'
+my_phone = '06'
+my_favorite = True
+annuaire = {}
+
+contact = create_contact("Bob", "08", False)
+add_contact(contact)
+
+contact = create_contact("Zulu", "09", True)
+add_contact(contact)
+
+contact = create_contact(my_name, my_phone, my_favorite)
+add_contact(contact)
+
+
+
+
+print(annuaire)
+print(get_names())
